85f57b8fc86924d1de3013f3efc50dcbdeb68503
Daniel Luque <yodaaniel123@gmail.com>
2024-11-07 16:31:32 -0400

    deploy v1

--- db/dbconection.js
+++ db/dbconection.js
@@ -1,20 +1,21 @@
-const pg = require('pg');
+//const pg = require('pg');
 //configurando pg
-// const pool=new pg.Pool({
-//     connectionString:'postgres://fl0user:MKaA5RtcvHu7@ep-plain-frog-81092153.ap-southeast-1.aws.neon.tech:5432/taller?sslmode=require',
-//     ssl:true
-// });
 
-const {Pool} = require('pg');
 
-const configBD = {
-    user:'postgres',
-    password:'postgres',
-    host:'localhost',
-    database:'taller',
-    port:5432
- }
+const { Pool } = require('pg');
+const pool=new Pool({
+    connectionString:process.env.URL_POSTGRESQL_NEON,
+    ssl:true
+});
 
-const pool = new Pool(configBD);
+// const configBD = {
+//     user: 'postgres',
+//     password: 'postgres',
+//     host: 'localhost',
+//     database: 'taller',
+//     port: 5432
+// }
+
+// const pool = new Pool(configBD);
 
 module.exports = pool;
